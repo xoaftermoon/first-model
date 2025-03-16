bfe0258dcd6119d0afeeeeb371e5689bd1fdbb0a
xoaftermoon <dae2eong@gmail.com>
2025-03-16 20:03:27 +0900

    study

---
+++ practice.py
@@ -0,0 +1,59 @@
+print ('hello world')
+print(5)
+print(-10)
+print(1000)
+print(5+3)
+print(2*5)
+print(20*(150+3))
+
+
+print('별풍선')
+print("어이없음")
+print("zzz*3")
+print("ㅋㅋㅋ"*3)
+
+#참 / 거짓
+print(5>10)
+print(50<60)
+print(True)
+print(not True)
+print(not (10<5))
+
+# 애완동물을 소개해 주세요~
+name = "만두똥만두"
+animal = "늙은개"
+age = 13
+hobby = "잠자기"
+is_addult = age >= 10
+
+print("우리집" +animal+ "는" +name+ "입니다")
+hobby = "재채기하기"
+
+#print(name+ "는"+hobby+"을아주좋아해요")
+'''주석입니다
+
+
+
+'''
+
+
+print(name+ "는"+hobby+"을아주좋아해요")
+age= 2  
+is_addult = age <2
+print(name+ "는" +str(age)+ "살입니다")
+print(name,"는어른일까요?" ,str(is_addult))
+
+#변수를 이용한 다음문장을 출력하겠습니다.
+
+station =  "사당", "신도림" , "인천공항"
+for s in station:
+    print(s+"행 열차가들어오고 있습니다")
+    
+
+
+
+
+
+
+
+
